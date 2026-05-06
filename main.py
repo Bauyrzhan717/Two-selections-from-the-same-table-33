a6bf2cde931e295800d8f2e25ac8fd23aefba52e
Твоё Имя <@gmail.com>
2026-05-06 15:30:20 +0500

    add sampling task (variant 33)

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+df=pd.read_csv("population.csv.py")
+true_mean=df["value"].mean()
+simple_mean=df.sample(n=20)["value"].mean()
+strat_mean=df.groupby("stratum")\
+    .apply(lambda x: x.sample(n=10))\
+    .reset_index(drop=True)["value"].mean()
+print("Ақиқат:",true_mean)
+print("Жай:",simple_mean)
+print("Стратифицировтік:",strat_mean)
+plt.bar(["True","Simple","Stratified"],
+        [true_mean,simple_mean,strat_mean])
+plt.savefig("result.png")
